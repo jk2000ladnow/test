e83e8415c5e67e0854d488e0cf2f50827c61c035
JasonHuang <jk2000ladnow@gmail.com>
2018-08-01 17:08:47 +0800

    i18n 1.1

--- I18N/I18nProperties.java
+++ I18N/I18nProperties.java
@@ -7,10 +7,8 @@ import java.util.ResourceBundle;
 public class I18nProperties {
 
 	public static void main(String[] args) {
-		I18nProperties self = new I18nProperties();
-		self.runEnUs();
-		System.out.println();
-		self.runZhTw();
+		runMessage("tw","JoJo");
+		runMessage("us");
 	}
 	
 	public void runEnUs () {
@@ -28,9 +26,36 @@ public class I18nProperties {
 		String good = bundle.getString("jason.good");
 		MessageFormat helloMf = new MessageFormat(hello);
 		MessageFormat goodMf = new MessageFormat(good);
-		Object[] obj = {"gg", "ininder"};
+		Object[] obj = {"Jason"};
 		System.out.println(helloMf.format(obj));
 		System.out.println(goodMf.format(obj));
 	}
+	
+	public static void runMessage (String country, String name) {
+		Locale local = null;
+		if ("TW".equals(country.toUpperCase())) {
+			local = Locale.TAIWAN;
+		} else if ("US".equals(country.toUpperCase())) {
+			local = Locale.US;
+		} else {
+			System.out.println("未支援該語言!!");
+			return;
+		}
+		
+		ResourceBundle bundle = ResourceBundle.getBundle("properties.message", local);
+		String hello = bundle.getString("jason.hello");
+		String good = bundle.getString("jason.good");
+		MessageFormat helloMf = new MessageFormat(hello);
+		MessageFormat goodMf = new MessageFormat(good);
+		Object[] obj = {name};
+		System.out.println(helloMf.format(obj));
+		System.out.println(goodMf.format(obj));
+		System.out.println();
+	}
+	
+	public static void runMessage (String country) {
+		runMessage(country,"");
+	}
+	
 
 }
