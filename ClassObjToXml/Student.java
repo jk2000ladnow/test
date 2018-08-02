a8df9c5508ef11c8b743478ae3a921ff2b94e141
JasonHuang <jk2000ladnow@gmail.com>
2018-08-02 10:22:50 +0800

    obj to xml single

---
+++ ClassObjToXml/Student.java
@@ -0,0 +1,57 @@
+package com.jason.test;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "Student")
+public class Student implements Serializable {
+	/** * */
+	private static final long serialVersionUID = -8317239764136972094L;
+	private String name;
+	private String country;
+	private String birthDate;
+	
+	public Student () {
+		
+	}
+	
+	public Student (String name, String country, String birthDate) {
+		this.birthDate = birthDate;
+		this.country = country;
+		this.name = name;
+	}
+	
+
+	/** * @param name * the name to set */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	/** * @param country * the country to set */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	
+	/** * @param birthDate the birthDate to set */
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+	
+}
---
+++ ClassObjToXml/XmlTransfer.java
@@ -0,0 +1,50 @@
+package com.jason.test;
+
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+public class XmlTransfer {
+
+	public static void main(String[] args) throws Exception {
+		ObjToXml();
+		XmlToObj();
+	}
+
+	public static void ObjToXml() throws ParseException {
+		JAXBContext ctx;
+		try {
+			ctx = JAXBContext.newInstance(Student.class);
+			Marshaller marshaller = ctx.createMarshaller();
+			// 格式化輸出
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			// 設置輸出編碼,默認為UTF-8 
+			marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+			Student stu = new Student("Jason","ROC","02/08");
+			File xmlFile = new File("C:/Student.xml");
+			marshaller.marshal(stu, xmlFile);
+			System.out.println("ObjToXml  Done!");
+		} catch (JAXBException e) {
+			System.out.println("ObjToXml  error");
+			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
+		}
+	}
+
+	public static void XmlToObj() {
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(Student.class);
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+			File file = new File("C:\\Student.xml");
+			Student stu = (Student) unmarshaller.unmarshal(file);
+			System.out.println(stu.getName() + "..." + stu.getCountry() + "..." +stu.getBirthDate());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
